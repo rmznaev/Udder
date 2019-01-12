7589c884e147acb0673174d7ec936da80c5ea71e
Ramazan Abdullayev <ramazan.aev@gmail.com>
2019-01-12 20:14:51 +0400

    Initial Commit

---
+++ UdderAgain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UdderAgain
+//
+//  Created by Ramazan Abdullayev on 1/12/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ UdderAgainTests/UdderAgainTests.swift
@@ -0,0 +1,34 @@
+//
+//  UdderAgainTests.swift
+//  UdderAgainTests
+//
+//  Created by Ramazan Abdullayev on 1/12/19.
+//  Copyright © 2019 Ramazan Abdullayev. All rights reserved.
+//
+
+import XCTest
+@testable import UdderAgain
+
+class UdderAgainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
